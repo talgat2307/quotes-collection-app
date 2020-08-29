7263aa049f62896adf10190de7437a4d3984e080
Talgat Muratbekov <talgat2307@gmail.com>
2020-08-29 18:46:18 +0600

    #2 - Added all imports for proper routing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import Home from './containers/Home/Home';
+import StarWars from './containers/Categories/StarWars';
+import FamousPeople from './containers/Categories/FamousPeople';
+import Motivational from './containers/Categories/Motivational';
+import Humour from './containers/Categories/Humour';
+import Saying from './containers/Categories/Saying';
+import Add from './containers/Add/Add';
+import Nav from './containers/Nav';
+import Menu from './containers/Menu';
+import Edit from './containers/Edit/Edit';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Nav/>
+        <div className='main-block'>
+          <Menu/>
+          <Switch>
+            <Route path='/' exact component={Home}/>
+            <Route path='/quotes/star-wars' component={StarWars}/>
+            <Route path='/quotes/famous-people' component={FamousPeople}/>
+            <Route path='/quotes/motivational' component={Motivational}/>
+            <Route path='/quotes/humour' component={Humour}/>
+            <Route path='/quotes/saying' component={Saying}/>
+            <Route path='/add-quote' component={Add}/>
+            <Route path='/quotes/:id/edit' component={Edit} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     </div>
   );
-}
+};
 
 export default App;
